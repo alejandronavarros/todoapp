61bf6159285f5f2cb1e568591598f689e121b754
alejandronavarros <alejandro_efese_ct@hotmail.es>
2021-07-19 21:21:44 +0200

    Base del programa

---
+++ frontend/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 3rd party imports
+import Auth from '@okta/okta-vue'
+
+// our own imports
+import Dashboard from '@/components/Dashboard'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-97100115.okta.com/oauth2/default',
+  client_id: '0oalvtm6zOV1gzsdj5d6',
+  redirect_uri: 'https://localhost:5001/implicit/callback',
+  scope: 'openid profile email'
+})
+
+Vue.use(Router)
+
+let router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Dashboard',
+      component: Dashboard,
+      props: true
+    },
+    {
+      path: '/implicit/callback',
+      component: Auth.handleCallback()
+    },
+  ]
+})
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+export default router
