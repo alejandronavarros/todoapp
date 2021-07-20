2be76d0344ad269225eb9db2f6f969d616c43b79
alejandronavarros <alejandro_efese_ct@hotmail.es>
2021-07-20 14:10:09 +0200

    Mitad del tutorial

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,37 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-// 3rd party imports
+import store from '../store/index.js'
+import Dashboard from '../components/Dashboard.vue'
+// Import the Okta Vue SDK
 import Auth from '@okta/okta-vue'
-
-// our own imports
-import Dashboard from '@/components/Dashboard'
-
-Vue.use(Auth, {
-  issuer: 'https://dev-97100115.okta.com/oauth2/default',
-  client_id: '0oalvtm6zOV1gzsdj5d6',
-  redirect_uri: 'https://localhost:5001/implicit/callback',
-  scope: 'openid profile email'
-})
 
 Vue.use(Router)
 
-let router = new Router({
+// Add the $auth plugin from the Okta Vue SDK to the Vue instance
+Vue.use(Auth, {
+  // Replace this with your Okta domain:
+  issuer: 'https://dev-97100115/oauth2/default',
+  // Replace this with the client ID of the Okta app you just created:
+  client_id: '0oa1a54qw9rZaGgvE5d7',
+  redirect_uri: 'http://localhost:5000/implicit/callback',
+  scope: 'openid profile email'
+})
+
+const router = new Router({
   mode: 'history',
+  base: __dirname,
   routes: [
-    {
-      path: '/',
-      name: 'Dashboard',
-      component: Dashboard,
-      props: true
-    },
-    {
-      path: '/implicit/callback',
-      component: Auth.handleCallback()
-    },
+    { path: '/', component: Dashboard },
+    // Handle the redirect from Okta using the Okta Vue SDK
+    { path: '/implicit/callback', component: Auth.handleCallback() },
   ]
 })
 
+// Check the authentication status before router transitions
 router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
 
 export default router
