c122f03abf5b6c8ec70e997fb75146c06ef72201
alejandronavarros <alejandro_efese_ct@hotmail.es>
2021-07-30 12:48:37 +0200

    Problema con DbContext

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './components/App'
+import router from './router'
+import store from './store'
+import { sync } from 'vuex-router-sync'
 
-Vue.config.productionTip = false;
+// Sync Vue router and the Vuex store
+sync(store, router)
 
 new Vue({
+  el: '#app',
+  store,
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  template: '<App/>',
+  components: { App }
+})
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,30 +1,29 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import store from '../store'
+import Dashboard from '../components/Dashboard'
+import Auth from '@okta/okta-vue'
 
-Vue.use(VueRouter);
+Vue.use(Router)
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
+Vue.use(Auth, {
+  issuer: 'https://dev-97100115.okta.com/oauth2/default',
+  client_id: '0oa1a54qw9rZaGgvE5d7',
+  redirect_uri: 'http://localhost:5000/implicit/callback',
+  scope: 'openid profile email'
+})
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
-});
+const router = new Router({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Dashboard },
+    // Handle the redirect from Okta using the Okta Vue SDK
+    { path: '/implicit/callback', component: Auth.handleCallback() },
+  ]
+})
 
-export default router;
+// Check the authentication status before router transitions
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+export default router
